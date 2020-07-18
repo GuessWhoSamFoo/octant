75c46efc3a1d0a198667d67b6c2f20d153e66e7f
GuessWhoSamFoo <foos@vmware.com>
2020-07-17 18:53:34 -0700

    Check content response before using showing modal for kubeconfig
    Signed-off-by: GuessWhoSamFoo <foos@vmware.com>

--- web/src/app/modules/sugarloaf/components/smart/uploader/uploader.component.ts
+++ web/src/app/modules/sugarloaf/components/smart/uploader/uploader.component.ts
@@ -2,34 +2,49 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, AfterViewInit } from '@angular/core';
 import { WebsocketService } from '../../../../shared/services/websocket/websocket.service';
 import { Subscription } from 'rxjs';
+import { ContentService } from '../../../../shared/services/content/content.service';
 
 @Component({
   selector: 'app-uploader',
   templateUrl: './uploader.component.html',
   styleUrls: ['./uploader.component.scss'],
 })
-export class UploaderComponent implements OnInit, OnDestroy {
+export class UploaderComponent implements OnInit, OnDestroy, AfterViewInit {
   inputValue: string;
   showModal: boolean;
+  contentCount = 0;
 
   private contentSubscription: Subscription;
 
-  constructor(private websocketService: WebsocketService) {}
+  constructor(
+    private websocketService: WebsocketService,
+    private contentService: ContentService
+  ) {}
 
   ngOnInit(): void {
+    this.contentSubscription = this.contentService.current.subscribe(
+      contentResponse => {
+        this.contentCount++;
+      }
+    );
+
     this.websocketService.registerHandler('event.octant.dev/loading', () => {
-      this.showModal = true;
+      this.showModal = this.contentCount === 1;
     });
     this.websocketService.registerHandler('event.octant.dev/refresh', () => {
       setTimeout(window.location.reload.bind(window.location), 1000);
     });
+  }
 
-    this.websocketService.sendMessage('action.octant.dev/loading', {
-      loading: true,
-    });
+  ngAfterViewInit(): void {
+    if (this.contentCount === 1) {
+      this.websocketService.sendMessage('action.octant.dev/loading', {
+        loading: true,
+      });
+    }
   }
 
   ngOnDestroy(): void {
